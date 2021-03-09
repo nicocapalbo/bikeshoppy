ae2ff6d3fc34d1364c2b58f4eac62a91076b0e37
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-03-09 16:38:33 +0100

    order creation + customization table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,28 @@
 class OrdersController < ApplicationController
   def new
-    @order = current_user.orders.new
+    @order = Order.new
     @bike = Bike.find(params[:bike])
-    @order.bike = @bike
     @customization = Customization.new
   end
 
   def create
+    @order = current_user.orders.new(order_params)
+    if @order.save
+      prms = JSON.parse(option_params.to_json)
+      prms.each_value { |value| Customization.create!(order: @order, option_id: value.to_i) }
+      redirect_to order_path(@order)
+    else
+      render new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:bike_id)
+  end
+
+  def option_params
+    params.require(:options)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :bike
   has_many :customizations
+  accepts_nested_attributes_for :customizations, reject_if: :all_blank, allow_destroy: true
 end
