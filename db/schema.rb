f6e501fd64ea520af4592089dc0cc1baf5f724ee
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-03-16 10:20:32 +0100

    default fix

--- db/schema.rb
+++ db/schema.rb
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_144443) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "role"
+    t.integer "role", 
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
